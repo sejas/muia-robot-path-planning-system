7ef888d715314d96eff39f7fdddceb45d50a785b
sejas <antonio@sejas.es>
2020-01-11 22:12:50 +0100

    feat: show the current color clicked

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,44 @@ const App: React.FC = () => {
   const [imageFile, setImageFile] = useState<File|null>(null)
   const [imagePreviewUrl, setImagePreviewUrl] = useState('https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Labirinto_003.svg/860px-Labirinto_003.svg.png')
   const [imageData, setImageData] = useState<Uint8ClampedArray>()
-  const [click1, setClick1] = useState({x:0,y:0})
+  const [click1, setClick1] = useState({x:-1,y:-1})
+  const [click2, setClick2] = useState({x:-1,y:-1})
+  const [countClicks, setCountClicks] = useState(0)
+  const incrementClick = () => setCountClicks(countClicks+1)
 
   const img = new Image()
   img.setAttribute('crossOrigin', 'anonymous')
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  useEffect(()=>{
-    // READ IMAGE AND SHOW IT IN CANVAS
+  const getCanvasCtx = ():[HTMLCanvasElement|null, CanvasRenderingContext2D|null] => {
     const canvas = canvasRef.current
-    if( canvas && imagePreviewUrl){
+    if( canvas){
       const ctx = canvas.getContext("2d")
-      img.src = imagePreviewUrl
-      if(ctx && img){
-        setTimeout(()=>{
-          const scale = canvas.width / img.width;
-          canvas.height = scale*img.height
-          ctx.clearRect(0,0, canvas.width, canvas.height)
-          ctx.setTransform(scale, 0, 0, scale, 0, 0);
-          ctx.drawImage(img, 0, 0)
-          const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
-          setImageData(imgData)
-        }, 500)
+      if(ctx){
+        return [canvas, ctx]
       }
     }
-  }, [imagePreviewUrl, img])
+    return [null, null]
+  }
+
+  const updateImageCanvas = (imagePreviewUrlParam='') => {
+    // READ IMAGE AND SHOW IT IN CANVAS
+    const [canvas, ctx] = getCanvasCtx()
+    if(canvas && ctx && imagePreviewUrl){
+        img.src = imagePreviewUrlParam || imagePreviewUrl
+        canvas.width = window.innerWidth*0.9
+        setTimeout(()=>{
+          const scale = canvas.width / img.width
+          canvas.height = scale*img.height
+          ctx.clearRect(0,0, canvas.width, canvas.height)
+          // ctx.setTransform(scale, 0, 0, scale, 0, 0);
+          ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+          setImageData(imageData)
+          // console.log(JSON.stringify(imageData))
+        }, 500)
+    }
+  }
+  useEffect(updateImageCanvas, [])
 
   const handleImageChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -44,23 +58,35 @@ const App: React.FC = () => {
     const file = files && files[0]
     reader.onloadend = () => {
       // setFile(file)
-      if('string' === typeof reader.result)
+      if('string' === typeof reader.result){
+        // const imagePreviewUrl = reader.result
         setImagePreviewUrl(reader.result)
         setImageFile(file)
+        updateImageCanvas(reader.result)
+      }
     }
     file && reader.readAsDataURL(file)
   }
 
+  useEffect(()=>{
+    console.log(countClicks)
+    const [canvas, ctx] = getCanvasCtx()
+    if (canvas && ctx && click1.x > 0){
+      const startX = click1.x - canvas.offsetLeft
+      const startY = click1.y - canvas.offsetTop
+      const pixelData = ctx.getImageData(startX, startY, 1,1).data
+      // ctx.fillRect(startX-1,startY-1,2,2) // DEBUG
+      console.log('pixelData!!', pixelData)
+    }
+  },[click1, click2])
+
   const clickImage = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     e.preventDefault();
-    const canvas = canvasRef.current
-    if (canvas){
-      setClick1({
-        x: e.clientX - canvas.offsetLeft,
-        y:e.clientY - canvas.offsetLeft
-      })
-    }
-    console.log(click1)
+    incrementClick()
+    setClick1({
+      x: e.clientX,
+      y: e.clientY
+    })
   }
 
   return (
@@ -77,10 +103,10 @@ const App: React.FC = () => {
             />
         </form>
         <canvas onClick={clickImage} ref={canvasRef} id="image-canvas"></canvas>
-        {/* {click1.x > 0 && <div className="point-click" style={{...POINT,
+        {click1.x > 0 && <div className="point-click" style={{...POINT,
           top: click1.y-POINT_SIZE/2,
           left: click1.x-POINT_SIZE/2,
-        }}></div>} */}
+        }}></div>}
       </main>
     </div>
   );
