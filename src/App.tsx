e19e6d9ed87dae1c2af7d92e4ff65da827e193e3
sejas <antonio@sejas.es>
2020-01-11 20:32:22 +0100

    feat: move the image to canvas add autoload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 const POINT_SIZE = 10
@@ -10,25 +9,58 @@ const POINT = {
 
 const App: React.FC = () => {
   // const [file, setFile] = useState('')
-  const [imagePreviewUrl, setImagePreviewUrl] = useState('')
+  const [imageFile, setImageFile] = useState<File|null>(null)
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Labirinto_003.svg/860px-Labirinto_003.svg.png')
+  const [imageData, setImageData] = useState<Uint8ClampedArray>()
   const [click1, setClick1] = useState({x:0,y:0})
+
+  const img = new Image()
+  img.setAttribute('crossOrigin', 'anonymous')
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  useEffect(()=>{
+    // READ IMAGE AND SHOW IT IN CANVAS
+    const canvas = canvasRef.current
+    if( canvas && imagePreviewUrl){
+      const ctx = canvas.getContext("2d")
+      img.src = imagePreviewUrl
+      if(ctx && img){
+        setTimeout(()=>{
+          const scale = canvas.width / img.width;
+          canvas.height = scale*img.height
+          ctx.clearRect(0,0, canvas.width, canvas.height)
+          ctx.setTransform(scale, 0, 0, scale, 0, 0);
+          ctx.drawImage(img, 0, 0)
+          const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+          setImageData(imgData)
+        }, 500)
+      }
+    }
+  }, [imagePreviewUrl, img])
+
   const handleImageChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    let reader = new FileReader()
+    const reader = new FileReader()
     const {files} = e.target
     const file = files && files[0]
     reader.onloadend = () => {
       // setFile(file)
       if('string' === typeof reader.result)
         setImagePreviewUrl(reader.result)
+        setImageFile(file)
     }
     file && reader.readAsDataURL(file)
   }
 
-  const clickImage = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+  const clickImage = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     e.preventDefault();
-    setClick1({x: e.clientX, y:e.clientY})
-    console.log(e)
+    const canvas = canvasRef.current
+    if (canvas){
+      setClick1({
+        x: e.clientX - canvas.offsetLeft,
+        y:e.clientY - canvas.offsetLeft
+      })
+    }
+    console.log(click1)
   }
 
   return (
@@ -40,15 +72,15 @@ const App: React.FC = () => {
       <form>
           <input className="fileInput" 
             type="file" 
-            onChange={handleImageChange} />
+            onChange={handleImageChange}
+            accept = "image/*"
+            />
         </form>
-        <div className="imgPreview">
-          {imagePreviewUrl && <img onClick={clickImage} src={imagePreviewUrl} alt="map" />}
-        </div>
-        {click1.x > 0 && <div className="point-click" style={{...POINT,
+        <canvas onClick={clickImage} ref={canvasRef} id="image-canvas"></canvas>
+        {/* {click1.x > 0 && <div className="point-click" style={{...POINT,
           top: click1.y-POINT_SIZE/2,
           left: click1.x-POINT_SIZE/2,
-        }}></div>}
+        }}></div>} */}
       </main>
     </div>
   );
