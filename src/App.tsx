2c6f908d19c113287a253bb7af6f74606d27028b
sejas <antonio@sejas.es>
2020-01-11 18:28:00 +0100

    feat: pick and show an image

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  // const [file, setFile] = useState('')
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('')
+  const handleImageChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    let reader = new FileReader()
+    const {files} = e.target
+    const file = files && files[0]
+    reader.onloadend = () => {
+      // setFile(file)
+      if('string' === typeof reader.result)
+        setImagePreviewUrl(reader.result)
+    }
+    file && reader.readAsDataURL(file)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+      <form>
+          <input className="fileInput" 
+            type="file" 
+            onChange={handleImageChange} />
+        </form>
+        <div className="imgPreview">
+          {imagePreviewUrl && <img src={imagePreviewUrl} alt="map" />}
+        </div>
+      </main>
     </div>
   );
 }
