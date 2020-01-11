a03b75db25446d4de968df0df4aa685a987fcd33
sejas <antonio@sejas.es>
2020-01-11 18:50:07 +0100

    feat: draw start point on click

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const POINT_SIZE = 10
+const POINT = {
+  width: POINT_SIZE,
+  height: POINT_SIZE
+}
+
 const App: React.FC = () => {
   // const [file, setFile] = useState('')
   const [imagePreviewUrl, setImagePreviewUrl] = useState('')
+  const [click1, setClick1] = useState({x:0,y:0})
   const handleImageChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     let reader = new FileReader()
@@ -16,6 +23,12 @@ const App: React.FC = () => {
         setImagePreviewUrl(reader.result)
     }
     file && reader.readAsDataURL(file)
+  }
+
+  const clickImage = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    e.preventDefault();
+    setClick1({x: e.clientX, y:e.clientY})
+    console.log(e)
   }
 
   return (
@@ -30,8 +43,12 @@ const App: React.FC = () => {
             onChange={handleImageChange} />
         </form>
         <div className="imgPreview">
-          {imagePreviewUrl && <img src={imagePreviewUrl} alt="map" />}
+          {imagePreviewUrl && <img onClick={clickImage} src={imagePreviewUrl} alt="map" />}
         </div>
+        {click1.x > 0 && <div className="point-click" style={{...POINT,
+          top: click1.y-POINT_SIZE/2,
+          left: click1.x-POINT_SIZE/2,
+        }}></div>}
       </main>
     </div>
   );
